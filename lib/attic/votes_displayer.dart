f51a2956df0db08b5d1b46733c510db9de9e8752
Alexis FREMONT <contact@1-2-7-8.tv>
2022-07-08 02:34:53 +0200

    Update votes_displayer.dart

--- lib/attic/votes_displayer.dart
+++ lib/attic/votes_displayer.dart
@@ -7,18 +7,37 @@ import 'json_transcoder.dart';
 class OpenAssembleeVoteDisplayer {
   List<IndividualVotes> votesAssemblyTest = [];
 
-  void getVotes(String path) async {
+  Future<bool> getVotes(String path) async {
     votesAssemblyTest =
         await OpenAssembleeJsonTranscoder().getJsonIndividualVotes(path);
+    return true;
   }
 
   Widget DrawVoteHemicycle(String path) {
-    getVotes(path);
-    return Center(
-      child: DrawHemicycle(votesAssemblyTest.length,
-          assemblyWidth: 0.75,
-          nbRows: (votesAssemblyTest.length / 25).ceil(),
-          individualVotes: votesAssemblyTest),
+    return FutureBuilder(
+      future: getVotes(path),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return CircularProgressIndicator();
+        }
+        if (snapshot.hasData) {
+          return Center(
+            child: DrawHemicycle(votesAssemblyTest.length,
+                assemblyWidth: 0.75,
+                nbRows: (votesAssemblyTest.length / 25).ceil(),
+                individualVotes: votesAssemblyTest),
+          );
+        }
+        if (snapshot.hasError) {
+          return Container(
+            child: Text(
+              snapshot.error.toString(),
+            ),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
   }
 }
