76bfc8bc3c03c650e040d894e3fe1535a875169a
Alexis FREMONT <contact@1-2-7-8.tv>
2022-07-08 02:31:33 +0200

    Update votes_displayer.dart

--- lib/attic/votes_displayer.dart
+++ lib/attic/votes_displayer.dart
@@ -4,37 +4,21 @@ import 'package:hemicycle/hemicycle.dart';
 
 import 'json_transcoder.dart';
 
-class OpenAssembleeVoteDisplayer extends StatefulWidget {
-  @override
-  _OpenAssembleeVoteDisplayerState createState() =>
-      _OpenAssembleeVoteDisplayerState();
-}
-
-class _OpenAssembleeVoteDisplayerState
-    extends State<OpenAssembleeVoteDisplayer> {
+class OpenAssembleeVoteDisplayer {
   List<IndividualVotes> votesAssemblyTest = [];
 
-  void getVotes() async {
-    votesAssemblyTest = await OpenAssembleeJsonTranscoder()
-        .getJsonIndividualVotes("assets/example_json/VTANR5L15V4417.json");
-    setState(() {
-      //
-    });
+  void getVotes(String path) async {
+    votesAssemblyTest =
+        await OpenAssembleeJsonTranscoder().getJsonIndividualVotes(path);
   }
 
   Widget DrawVoteHemicycle(String path) {
-    getVotes();
+    getVotes(path);
     return Center(
       child: DrawHemicycle(votesAssemblyTest.length,
           assemblyWidth: 0.75,
           nbRows: (votesAssemblyTest.length / 25).ceil(),
           individualVotes: votesAssemblyTest),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
   }
 }
