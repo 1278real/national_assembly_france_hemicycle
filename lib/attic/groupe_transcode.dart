6266eefe9f42806f2fbafc7f4638ea295cbe3bba
Alexis FREMONT <contact@1-2-7-8.tv>
2022-07-09 12:08:14 +0200

    Update groupe_transcode.dart

--- lib/attic/groupe_transcode.dart
+++ lib/attic/groupe_transcode.dart
@@ -12,7 +12,6 @@ class GroupTranscode {
 
 /// Legislature XV : 2017-2022
 List<GroupTranscode> groupsLegis15 = [
-  GroupTranscode(0, "PO723569", "NI", nonInscritGris),
   GroupTranscode(1, "PO730958", "LFI", nupesViolet),
   GroupTranscode(2, "PO730940", "GDR", nupesRouge),
   GroupTranscode(3, "PO758835", "SOC", nupesRose),
@@ -22,19 +21,20 @@ List<GroupTranscode> groupsLegis15 = [
   GroupTranscode(7, "PO771923", "AGIR", agirBleu),
   GroupTranscode(8, "PO730934", "LR", republicainsBleu),
   GroupTranscode(9, "PO771889", "UDI", udiBleu),
+  GroupTranscode(99, "PO723569", "NI", nonInscritGris),
 ];
 
 /// Legislature XVI : 2022-...
 List<GroupTranscode> groupsLegis16 = [
-  GroupTranscode(0, "...", "NI", nonInscritGris),
   GroupTranscode(1, "...", "LFI", nupesViolet),
-  GroupTranscode(2, "...", "GDR", nupesRouge),
-  GroupTranscode(3, "...", "EELV", nupesVert),
+  GroupTranscode(2, "...", "EELV", nupesVert),
+  GroupTranscode(3, "...", "GDR", nupesRouge),
   GroupTranscode(4, "...", "SOC", nupesRose),
   GroupTranscode(5, "...", "LIOMT", liomtRose),
   GroupTranscode(6, "...", "REN", renaissanceOrange),
-  GroupTranscode(7, "...", "MDM", modemJaune),
+  GroupTranscode(7, "...", "DEM", modemJaune),
   GroupTranscode(8, "...", "HRZ", horizonsBleu),
   GroupTranscode(9, "...", "LR-UDI", republicainsBleu),
   GroupTranscode(10, "...", "RN", rnBleu),
+  GroupTranscode(99, "...", "NI", nonInscritGris),
 ];
