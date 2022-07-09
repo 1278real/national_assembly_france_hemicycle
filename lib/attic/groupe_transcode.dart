b7c8be46f738cf5528c48e5110916ac47c823830
Alexis FREMONT <contact@1-2-7-8.tv>
2022-07-09 11:48:23 +0200

    Update groupe_transcode.dart

--- lib/attic/groupe_transcode.dart
+++ lib/attic/groupe_transcode.dart
@@ -12,29 +12,29 @@ class GroupTranscode {
 
 /// Legislature XV : 2017-2022
 List<GroupTranscode> groupsLegis15 = [
-  GroupTranscode(5, "PO730964", "LaREM", renaissanceOrange),
-  GroupTranscode(8, "PO730934", "LR", republicainsBleu),
-  GroupTranscode(6, "PO774834", "MoDem", modemJaune),
-  GroupTranscode(3, "PO758835", "SOC", nupesRose),
   GroupTranscode(0, "PO723569", "NI", nonInscritGris),
-  GroupTranscode(7, "PO771923", "AGIR", agirBleu),
-  GroupTranscode(9, "PO771889", "UDI", udiBleu),
-  GroupTranscode(4, "PO759900", "L&T", territoiresRose),
   GroupTranscode(1, "PO730958", "LFI", nupesViolet),
   GroupTranscode(2, "PO730940", "GDR", nupesRouge),
+  GroupTranscode(3, "PO758835", "SOC", nupesRose),
+  GroupTranscode(4, "PO759900", "L&T", territoiresRose),
+  GroupTranscode(5, "PO730964", "LaREM", renaissanceOrange),
+  GroupTranscode(6, "PO774834", "MoDem", modemJaune),
+  GroupTranscode(7, "PO771923", "AGIR", agirBleu),
+  GroupTranscode(8, "PO730934", "LR", republicainsBleu),
+  GroupTranscode(9, "PO771889", "UDI", udiBleu),
 ];
 
 /// Legislature XVI : 2022-...
 List<GroupTranscode> groupsLegis16 = [
-  GroupTranscode(6, "...", "REN", renaissanceOrange),
-  GroupTranscode(10, "...", "RN", rnBleu),
+  GroupTranscode(0, "...", "NI", nonInscritGris),
   GroupTranscode(1, "...", "LFI", nupesViolet),
-  GroupTranscode(9, "...", "LR-UDI", republicainsBleu),
-  GroupTranscode(4, "...", "SOC", nupesRose),
-  GroupTranscode(3, "...", "EELV", nupesVert),
   GroupTranscode(2, "...", "GDR", nupesRouge),
+  GroupTranscode(3, "...", "EELV", nupesVert),
+  GroupTranscode(4, "...", "SOC", nupesRose),
   GroupTranscode(5, "...", "LIOMT", liomtRose),
+  GroupTranscode(6, "...", "REN", renaissanceOrange),
   GroupTranscode(7, "...", "MDM", modemJaune),
   GroupTranscode(8, "...", "HRZ", horizonsBleu),
-  GroupTranscode(0, "...", "NI", nonInscritGris),
+  GroupTranscode(9, "...", "LR-UDI", republicainsBleu),
+  GroupTranscode(10, "...", "RN", rnBleu),
 ];
