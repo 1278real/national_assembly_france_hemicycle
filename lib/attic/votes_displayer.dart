dcdf1220eac14214ed5befc30167efd9c3469549
Alexis FREMONT <contact@1-2-7-8.tv>
2022-07-13 20:55:03 +0200

    Update votes_displayer.dart

--- lib/attic/votes_displayer.dart
+++ lib/attic/votes_displayer.dart
@@ -115,7 +115,7 @@ class OpenAssembleeVoteDisplayer {
           return Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.width *
-                ((withDivider ? 1.6 : 1.4) + (0.02 * titleString.length / 50)),
+                ((withDivider ? 1.4 : 1.2) + (0.02 * titleString.length / 50)),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
