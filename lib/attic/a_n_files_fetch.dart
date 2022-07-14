406903c044c324da9c1c0efa100bb6768ee7682d
Alexis FREMONT <contact@1-2-7-8.tv>
2022-07-14 13:01:37 +0200

    Update a_n_files_fetch.dart

--- lib/attic/a_n_files_fetch.dart
+++ lib/attic/a_n_files_fetch.dart
@@ -5,30 +5,34 @@ import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-bool checkPrefs() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool shouldUpdate = true;
-    String _lastFetched = (prefs.getString('NAF_lastFetched') ?? "");
-    
-    if (_lastFetched != "") {
+import 'helpers.dart';
+
+Future<bool> checkPrefs() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool shouldUpdate = true;
+  String _lastFetched = (prefs.getString('NAF_lastFetched') ?? "");
+
+  if (_lastFetched != "") {
     DateTime _lastFetchedTime = dateFormatter(_lastFetched);
-    if (_lastFetchedTime.isBefore(DateTime.now()) {
-    shouldUpdate = true;
+    if (_lastFetchedTime.isBefore(DateTime.now())) {
+      shouldUpdate = true;
     }
-    } else {
+  } else {
     shouldUpdate = true;
-    }
-    
-    return shouldUpdate;
   }
-  
+
+  return shouldUpdate;
+}
+
 void updatePrefs() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    
-    prefs.setString('NAF_lastFetched', dateStringFormatter(DateTime.now())).then((bool success) {
-      return value;
-    });
-  }
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  prefs
+      .setString('NAF_lastFetched', dateStringFormatter(DateTime.now()))
+      .then((bool success) {
+    return true;
+  });
+}
 
 /// ### *Download the ZIP archives* from National Assembly open data and *extract* in designated directory :
 ///
@@ -47,191 +51,191 @@ Future<bool> getUpdatedDatasFromAssembly(
     String pathToAmendements =
         "https://data.assemblee-nationale.fr/static/openData/repository/16/loi/amendements_div_legis/Amendements.json.zip",
     required Directory destinationDirectory}) async {
-    
-  if(checkPrefs()) {
-  if (destinationDirectory != null) {
-    HttpClient httpClient = new HttpClient();
+  bool needsUpdate = await checkPrefs();
+  if (needsUpdate) {
+    if (destinationDirectory != null) {
+      HttpClient httpClient = new HttpClient();
 
-    ///
-    ///
-    /// delete existing Files and Links in App Support Directory
-    ///
-    ///
+      ///
+      ///
+      /// delete existing Files and Links in App Support Directory
+      ///
+      ///
 
-    List<FileSystemEntity> initialListOfFiles =
-        await destinationDirectory.list(recursive: true).toList();
+      List<FileSystemEntity> initialListOfFiles =
+          await destinationDirectory.list(recursive: true).toList();
 
-    int numberOfFilesDeleted = 0;
-    for (FileSystemEntity file in initialListOfFiles) {
-      numberOfFilesDeleted += 1;
-      if (file is File) {
-        file.delete();
-        // print("FILES DELETED = " + numberOfFilesDeleted.toString());
+      int numberOfFilesDeleted = 0;
+      for (FileSystemEntity file in initialListOfFiles) {
+        numberOfFilesDeleted += 1;
+        if (file is File) {
+          file.delete();
+          // print("FILES DELETED = " + numberOfFilesDeleted.toString());
+        }
       }
-    }
-    for (FileSystemEntity file in initialListOfFiles) {
-      numberOfFilesDeleted += 1;
-      if (file is Link) {
-        file.delete();
-        // print("LINKS DELETED = " + numberOfFilesDeleted.toString());
+      for (FileSystemEntity file in initialListOfFiles) {
+        numberOfFilesDeleted += 1;
+        if (file is Link) {
+          file.delete();
+          // print("LINKS DELETED = " + numberOfFilesDeleted.toString());
+        }
       }
-    }
-    print("FILE ENTITIES DELETED = " + numberOfFilesDeleted.toString());
+      print("FILE ENTITIES DELETED = " + numberOfFilesDeleted.toString());
 
-    ///
-    ///
-    /// download ZIP "Dossiers Législatifs" from open data AN
-    ///
-    ///
+      ///
+      ///
+      /// download ZIP "Dossiers Législatifs" from open data AN
+      ///
+      ///
 
-    File dossiersFile;
-    String dossiersFilePath = '';
+      File dossiersFile;
+      String dossiersFilePath = '';
 
-    try {
-      var request = await httpClient.getUrl(Uri.parse(pathToDossiers));
-      var response = await request.close();
-      if (response.statusCode == 200) {
-        var bytes = await consolidateHttpClientResponseBytes(response);
-        dossiersFilePath = destinationDirectory.path + "/dossiers.zip";
-        dossiersFile = File(dossiersFilePath);
-        await dossiersFile.writeAsBytes(bytes);
-      } else {
-        dossiersFilePath = 'Error code: ' + response.statusCode.toString();
+      try {
+        var request = await httpClient.getUrl(Uri.parse(pathToDossiers));
+        var response = await request.close();
+        if (response.statusCode == 200) {
+          var bytes = await consolidateHttpClientResponseBytes(response);
+          dossiersFilePath = destinationDirectory.path + "/dossiers.zip";
+          dossiersFile = File(dossiersFilePath);
+          await dossiersFile.writeAsBytes(bytes);
+        } else {
+          dossiersFilePath = 'Error code: ' + response.statusCode.toString();
+        }
+      } catch (ex) {
+        dossiersFilePath = 'Can not fetch url';
       }
-    } catch (ex) {
-      dossiersFilePath = 'Can not fetch url';
-    }
 
-    ///
-    ///
-    /// extract ZIP "Dossiers Législatifs" to App Support Directory
-    ///
-    ///
+      ///
+      ///
+      /// extract ZIP "Dossiers Législatifs" to App Support Directory
+      ///
+      ///
 
-    await extractFileToDisk(
-        dossiersFilePath, destinationDirectory.path + "/docs_legis");
+      await extractFileToDisk(
+          dossiersFilePath, destinationDirectory.path + "/docs_legis");
 
-    initialListOfFiles =
-        await destinationDirectory.list(recursive: true).toList();
-    for (FileSystemEntity file in initialListOfFiles) {
-      if (file.path == dossiersFilePath) {
-        file.delete();
-        // print("FILES DELETED = " + numberOfFilesDeleted.toString());
+      initialListOfFiles =
+          await destinationDirectory.list(recursive: true).toList();
+      for (FileSystemEntity file in initialListOfFiles) {
+        if (file.path == dossiersFilePath) {
+          file.delete();
+          // print("FILES DELETED = " + numberOfFilesDeleted.toString());
+        }
       }
-    }
 
-    ///
-    ///
-    /// download ZIP "Votes" from open data AN
-    ///
-    ///
+      ///
+      ///
+      /// download ZIP "Votes" from open data AN
+      ///
+      ///
 
-    File votesFile;
-    String votesFilePath = '';
+      File votesFile;
+      String votesFilePath = '';
 
-    try {
-      var request = await httpClient.getUrl(Uri.parse(pathToVotes));
-      var response = await request.close();
-      if (response.statusCode == 200) {
-        var bytes = await consolidateHttpClientResponseBytes(response);
-        votesFilePath = destinationDirectory.path + "/votes.zip";
-        votesFile = File(votesFilePath);
-        await votesFile.writeAsBytes(bytes);
-      } else {
-        votesFilePath = 'Error code: ' + response.statusCode.toString();
+      try {
+        var request = await httpClient.getUrl(Uri.parse(pathToVotes));
+        var response = await request.close();
+        if (response.statusCode == 200) {
+          var bytes = await consolidateHttpClientResponseBytes(response);
+          votesFilePath = destinationDirectory.path + "/votes.zip";
+          votesFile = File(votesFilePath);
+          await votesFile.writeAsBytes(bytes);
+        } else {
+          votesFilePath = 'Error code: ' + response.statusCode.toString();
+        }
+      } catch (ex) {
+        votesFilePath = 'Can not fetch url';
       }
-    } catch (ex) {
-      votesFilePath = 'Can not fetch url';
-    }
 
-    ///
-    ///
-    /// extract ZIP "Dossiers Législatifs" to App Support Directory
-    ///
-    ///
+      ///
+      ///
+      /// extract ZIP "Dossiers Législatifs" to App Support Directory
+      ///
+      ///
 
-    await extractFileToDisk(
-        votesFilePath, destinationDirectory.path + "/votes");
+      await extractFileToDisk(
+          votesFilePath, destinationDirectory.path + "/votes");
 
-    initialListOfFiles =
-        await destinationDirectory.list(recursive: true).toList();
-    for (FileSystemEntity file in initialListOfFiles) {
-      if (file.path == votesFilePath) {
-        file.delete();
-        // print("FILES DELETED = " + numberOfFilesDeleted.toString());
+      initialListOfFiles =
+          await destinationDirectory.list(recursive: true).toList();
+      for (FileSystemEntity file in initialListOfFiles) {
+        if (file.path == votesFilePath) {
+          file.delete();
+          // print("FILES DELETED = " + numberOfFilesDeleted.toString());
+        }
       }
-    }
 
-    ///
-    ///
-    /// download ZIP "Amendements" from open data AN
-    ///
-    ///
+      ///
+      ///
+      /// download ZIP "Amendements" from open data AN
+      ///
+      ///
 
-    File amendementsFile;
-    String amendementsFilePath = '';
+      File amendementsFile;
+      String amendementsFilePath = '';
 
-    try {
-      var request = await httpClient.getUrl(Uri.parse(pathToAmendements));
-      var response = await request.close();
-      if (response.statusCode == 200) {
-        var bytes = await consolidateHttpClientResponseBytes(response);
-        amendementsFilePath = destinationDirectory.path + "/amendements.zip";
-        amendementsFile = File(amendementsFilePath);
-        await amendementsFile.writeAsBytes(bytes);
-      } else {
-        amendementsFilePath = 'Error code: ' + response.statusCode.toString();
+      try {
+        var request = await httpClient.getUrl(Uri.parse(pathToAmendements));
+        var response = await request.close();
+        if (response.statusCode == 200) {
+          var bytes = await consolidateHttpClientResponseBytes(response);
+          amendementsFilePath = destinationDirectory.path + "/amendements.zip";
+          amendementsFile = File(amendementsFilePath);
+          await amendementsFile.writeAsBytes(bytes);
+        } else {
+          amendementsFilePath = 'Error code: ' + response.statusCode.toString();
+        }
+      } catch (ex) {
+        amendementsFilePath = 'Can not fetch url';
       }
-    } catch (ex) {
-      amendementsFilePath = 'Can not fetch url';
-    }
 
-    ///
-    ///
-    /// extract ZIP "Dossiers Législatifs" to App Support Directory
-    ///
-    ///
+      ///
+      ///
+      /// extract ZIP "Dossiers Législatifs" to App Support Directory
+      ///
+      ///
 
-    await extractFileToDisk(
-        amendementsFilePath, destinationDirectory.path + "/amendements");
+      await extractFileToDisk(
+          amendementsFilePath, destinationDirectory.path + "/amendements");
 
-    initialListOfFiles =
-        await destinationDirectory.list(recursive: true).toList();
-    for (FileSystemEntity file in initialListOfFiles) {
-      if (file.path == amendementsFilePath) {
-        file.delete();
-        // print("FILES DELETED = " + numberOfFilesDeleted.toString());
+      initialListOfFiles =
+          await destinationDirectory.list(recursive: true).toList();
+      for (FileSystemEntity file in initialListOfFiles) {
+        if (file.path == amendementsFilePath) {
+          file.delete();
+          // print("FILES DELETED = " + numberOfFilesDeleted.toString());
+        }
       }
-    }
 
-    ///
-    ///
-    /// list directories and count files in App Support Directory
-    ///
-    ///
+      ///
+      ///
+      /// list directories and count files in App Support Directory
+      ///
+      ///
 
-    List<FileSystemEntity> listOfFiles =
-        await destinationDirectory.list(recursive: true).toList();
+      List<FileSystemEntity> listOfFiles =
+          await destinationDirectory.list(recursive: true).toList();
 
-    int numberOfFilesAdded = 0;
-    for (FileSystemEntity file in listOfFiles) {
-      numberOfFilesAdded += 1;
-      if (file is Directory) {
-        print(file.path);
+      int numberOfFilesAdded = 0;
+      for (FileSystemEntity file in listOfFiles) {
+        numberOfFilesAdded += 1;
+        if (file is Directory) {
+          print(file.path);
+        }
       }
+      print("FILES ADDED = " + numberOfFilesAdded.toString());
+
+      ///
+      ///
+      /// return TRUE for success
+      ///
+      ///
+
+      updatePrefs();
+
+      return true;
     }
-    print("FILES ADDED = " + numberOfFilesAdded.toString());
-
-    ///
-    ///
-    /// return TRUE for success
-    ///
-    ///
-    
-    updatePrefs();
-
-    return true;
-  }
   }
 
   return false;
